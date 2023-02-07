9db9c1d08eed7638710079976667689a42879f84
Serge <93369654+comoestassergio@users.noreply.github.com>
2023-02-07 19:07:18 +0300

    Create utility styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Header from './components/Header/Header'
 import './styles/App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Header />
     </div>
   )
 }
